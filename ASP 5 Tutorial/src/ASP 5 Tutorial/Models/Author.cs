789792853ef260d0ea1f746722b742bf820080d2
Cordell Wagendorf <dorf06@gmail.com>
2016-01-19 11:19:20 -0600

    Guess I havent committed in a while

---
+++ ASP 5 Tutorial/src/ASP 5 Tutorial/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_5_Tutorial.Models
+{
+    public class Author
+    {
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
